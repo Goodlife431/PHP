e88e9f22dc190e975ca9bbe0a8ad34261d4ce8b6
Goodlife <oduwolejohn431@gmail.com>
2023-07-07 01:39:15 +0100

    new

--- test.php
+++ test.php
@@ -38,6 +38,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
            $email = test_input($_POST["gender"]);
        }
 }
+//VALIDATE NAME
+$name = test_input($_POST["name"]);
+if(!preg_match("/^[a-zA-Z-']*$/", $name)){
+    $nameErr = "Only letters and white space allowed";
+}
+//VALIDATE EMAIL
+$email = test_input($_POST["email"]);
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $emailErr = "Invalid email format";
+}
+//VALIDATE URL
+$website = test_input($_POST["website"]);
+if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+  $websiteErr = "Invalid URL";
+}
 ?>
 
 <!DOCTYPE html>
