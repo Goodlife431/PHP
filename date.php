c6f7a5b98b8f44c86e77b08b4b9a3c180cfb089d
Goodlife <oduwolejohn431@gmail.com>
2023-07-09 17:03:55 +0100

    construtor

--- date.php
+++ date.php
@@ -1,7 +1,12 @@
+<!-- <?php
+echo "Today is " .date("Y/m/d") . "<br>";
+echo "Today is " .date("Y.m.d") . "<br>";
+echo "Today is " .date("Y-m-d") . "<br>";
+echo "Today is " .date("l") . "<br";
+echo "The time is " .date("h:i:sa") . "<br>";
+?> -->
+
 <?php
-// echo "Today is " .date("Y/m/d") . "<br>";
-// echo "Today is " .date("Y.m.d") . "<br>";
-// echo "Today is " .date("Y-m-d") . "<br>";
-// echo "Today is " .date("l") . "<br";
-&copy; 2010-<?php echo date("Y");?>
+date_default_timezone_set("America/New_York");
+echo "The time is " . date("h:i:sa");
 ?>
