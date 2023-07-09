abcbc836d734c05be65b6cb7a9c563de2fbf2f7a
Goodlife <oduwolejohn431@gmail.com>
2023-07-09 04:09:16 +0100

    time

---
+++ date.php
@@ -0,0 +1,7 @@
+<?php
+// echo "Today is " .date("Y/m/d") . "<br>";
+// echo "Today is " .date("Y.m.d") . "<br>";
+// echo "Today is " .date("Y-m-d") . "<br>";
+// echo "Today is " .date("l") . "<br";
+&copy; 2010-<?php echo date("Y");?>
+?>
