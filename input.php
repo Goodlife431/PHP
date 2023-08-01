ffb2882dae4678edca7701ce5b1616aaf20ef644
Goodlife <oduwolejohn431@gmail.com>
2023-08-01 12:58:14 +0100

    collecting input

--- input.php
+++ input.php
@@ -6,14 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="site.php" method="name">
+    <form action="connect.php" method="GET">
+        <br>
         Name: <input type="text" name="username">
         <br>
         Age: <input type="number" name="age">
         <input type="submit"> 
     </form>
     <br>
-    Your name is: <?php echo $_GET["Username"] ?>
+    Your name is: <?php echo $_GET["username"] ?>
     <br>
     Your age is: <?php echo $_GET["age"] ?>
 </body>
