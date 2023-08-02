2e20888c54b699284380c412c535d5e195c502ca
Goodlife <oduwolejohn431@gmail.com>
2023-08-02 15:12:13 +0100

    better calculator

---
+++ tutorial/better_calculator.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="better_calculator.php" method="post">
+        number 1: <input type="number" name="num1"> <br>
+        number 2: <input type="number" name="num2"> <br>
+        OP: <input type="text" name="op">
+        <input type="submit">
+    </form>
+
+    <?php 
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["op"];
+
+    if($op == "+"){
+        $num1 + $num2;
+    }elseif($op == "-"){
+        $num1 - $num2;
+    }elseif($op == "*"){
+        $num1 * $num2;
+    }elseif($op == "/"){
+        $num1 / $num2;
+    }else{
+        echo "Invalid operator";
+    }
+    ?>
+</body>
+</html>
