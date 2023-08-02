5c508093c8d669b5aa7a88bdd438f928b33744b3
Goodlife <oduwolejohn431@gmail.com>
2023-08-02 13:50:32 +0100

    function

---
+++ tutorial/function.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+     function sayhi($name, $age){
+        echo "Welcome to my new function";
+     };
+     
+     sayhi("seun", 21);
+     sayhi("Bimbo", 45);
+     sayhi("Sikiru", 55)
+    ?>
+</body>
+</html>
