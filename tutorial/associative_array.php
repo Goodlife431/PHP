7ddf329d887f87694dc976b1a8d5e7059d37bb3d
Goodlife <oduwolejohn431@gmail.com>
2023-08-02 13:36:14 +0100

    associative array

---
+++ tutorial/associative_array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="associatuve_array.php" method="post">
+        <input type="text" name="students"> <br>
+        <input type="submit">
+    </form>
+
+    <?php 
+    $grades = array("Jim" => "A+", "Kemi" => "A+", "Shola" => "C+", "Bimbo" => "B+");
+    echo $grades[$_POST["students"]];
+    ?>
+</body>
+</html>
