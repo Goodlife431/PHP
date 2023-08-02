1ea80a4b1231c3c2edfd4d4c0441e60a2fe976ce
Goodlife <oduwolejohn431@gmail.com>
2023-08-02 14:09:09 +0100

    return statement

--- test.php
+++ test.php
@@ -139,4 +139,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 ?>
-.
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  Email
+</body>
+</html>
