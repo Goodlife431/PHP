e87c6b92a1c642383a706a1a121a3ce9cbd051f1
Goodlife <oduwolejohn431@gmail.com>
2023-08-04 09:25:21 +0100

    associative for each

---
+++ concepts/codeholic/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    #loops 
+    #for each 
+
+    $fruits = ["Bananas", "Apples", "Oranges"];
+    foreach ($fruits as $i => $fruits) {
+        # code...
+        echo $i . " " . $fruits . "<br>";
+    }
+    ?>
+</body>
+</html>
