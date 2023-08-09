9809b0c950246bdbc6221b172d33b9d1f4fed325
Goodlife <oduwolejohn431@gmail.com>
2023-08-09 10:56:41 +0100

    include

---
+++ concepts/codeholic/website_skeleton/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="index.php">Home</a>
+        <a href="about.php">Home</a>
+    </header>
+    <h3> Abuja Wuse 5&#8451; </h3>
+    <h1> Welcome to my cool website </h1>
+
+    <footer>
+        Copyright &copy; The codeholic
+    </footer>
+</body>
+</html>
