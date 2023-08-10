6c2e1c7004547ab6ac2e037bc3ce1791df5f6d84
Goodlife <oduwolejohn431@gmail.com>
2023-08-10 10:56:08 +0100

    todos

--- codeholic/todolist_with_fs/newtodo.php
+++ codeholic/todolist_with_fs/newtodo.php
@@ -4,11 +4,22 @@
 // var_dump($_POST);
 // echo '<pre>';
 
-$todoname = $_POST['todo_name'] ?? '';
+$todoname = $_POST['todolist'] ?? '';
 $todoname = trim($todoname);
 
 if($todoname){
+    if(file_exists('todo.json')){
     $json = file_get_contents('todo.json');
-    echo $json;
+    $jsonarray = json_decode($json, true);
+    }else {
+        $jsonarray = [];
+    }
+    $jsonarray[$todoname] = ['completed' => false];echo $json;
+    
+    var_dump($jsonarray);
+
+    file_put_contents('todo.json', json_encode($jsonarray, JSON_PRETTY_PRINT));
 }
+
+header('location: index.php')
 ?>
