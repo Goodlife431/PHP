e631d4496877e01163bed090c9abc420cafb538d
Goodlife <oduwolejohn431@gmail.com>
2023-08-11 12:05:08 +0100

    OOP

--- codeholic/OOP/index.php
+++ codeholic/OOP/index.php
@@ -1,28 +1,6 @@
 <?php 
 
-class Person {
-    public String $name;
-    public int $age;
-    private float $salary;
-
-    public function __construct($name, $age, $salary)
-    {
-     $this->name = $name;
-     $this->age  = $age;
-     $this->salary = $salary; 
-    }
-
-    public function getSalary()
-    {
-      return $this->salary;  
-    }
-
-    public function setSalary($salary)
-    {
-     return $this->salary = $salary;   
-    }
-
-}
+require_once('./Person.php');
 
 $p = new Person('seun',21, 800000.00);
 
@@ -31,13 +9,6 @@ echo $p->age .'<br>';
 // echo $p->getSalary() .'<br>';
 $p->setSalary(1000000.00);
 echo $p->getSalary() .'<br>';
-
-
-
-
-
-
-
 
 
 ?>
