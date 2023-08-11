2870de3f0a50ea1ff26c80c6ef7ffb35bd9094de
Goodlife <oduwolejohn431@gmail.com>
2023-08-11 09:20:54 +0100

    done todolist with php files and little js script

--- codeholic/todolist_with_fs/change_status.php
+++ codeholic/todolist_with_fs/change_status.php
@@ -1,5 +1,18 @@
 <?php 
-echo '<pre>';
-var_dump($_POST);
-echo '<pre>';
+// echo '<pre>';
+// var_dump($_POST);
+// echo '<pre>';
+
+$json = file_get_contents('todo.json');
+$jsonarray = json_decode($json, true);
+
+$todoName = $_POST['todo_name'];
+if(isset($jsonarray[$todoName])){
+$jsonarray[$todoName]['completed'] = !$jsonarray[$todoName]['completed'];
+file_put_contents('todo.json', json_encode($jsonarray, JSON_PRETTY_PRINT));
+echo 'todo status updated successfully';
+}else {
+    echo 'Todo not found in Jsonarray';
+}
+header('location: index.php');
 ?>
