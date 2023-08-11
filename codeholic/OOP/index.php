0489f8a1f9701e20fc1baafb36362e1990e7870e
Goodlife <oduwolejohn431@gmail.com>
2023-08-11 13:35:40 +0100

    done with the basics of oop in php

--- codeholic/OOP/index.php
+++ codeholic/OOP/index.php
@@ -1,14 +1,18 @@
 <?php 
+require_once './Person.php';
+require_once './Student.php';
 
-require_once('./Person.php');
+// $p = new Person('seun',21, null);
 
-$p = new Person('seun',21, 800000.00);
-
-echo $p->name .'<br>';
-echo $p->age .'<br>';
+// echo $p->name .'<br>';
+// echo $p->age .'<br>';
+// // echo $p->getSalary() .'<br>';
+// $p->setSalary(1000000.00);
 // echo $p->getSalary() .'<br>';
-$p->setSalary(1000000.00);
-echo $p->getSalary() .'<br>';
 
+
+$s = new Student('Shola', 15, 31814);
+
+echo $s->name . '<br>';
 
 ?>
