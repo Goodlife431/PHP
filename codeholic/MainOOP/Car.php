37ba8bc3fcca254f462e17598c3f78c0afcc156b
Goodlife <oduwolejohn431@gmail.com>
2023-08-14 08:40:01 +0100

    new

---
+++ codeholic/MainOOP/Car.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Car{
+
+    private $color  = 'ge';
+    public $weight;
+    private $year;
+    private $available = ['red', 'white', 'blue', 'yellow'];
+
+    public function setYear($year) {
+ 
+        $this->$year = $year;   
+       }
+    public function getYear(){
+        return $this->year;
+    }
+    public function setColor($color){
+        if (in_array($color, $this->available)) {
+            $this->color = $color;
+        }
+    }
+    public function getColor(){
+        return $this->color;
+    }
+}
+
+$myCar = new Car();
+$myCar->setYear('2020');
+$myCar->setColor('green');
+
+echo $myCar->getColor()
+
+
+
+
+
+?>
