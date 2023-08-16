3cb4e6678b83514a650865089f4a1480a40bc596
Goodlife <oduwolejohn431@gmail.com>
2023-08-16 10:13:51 +0100

    autoloading and composer download

---
+++ codeholic/autoloading/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// require_once './app/Email.php';
+// require_once './app/Person.php';
+require_once './vendor/autoload.php';
+
+use app\Email;
+use app\Person;
+
+$email = new Email();
+
+$person = new app\Person();
+
+
+?>
---
+++ codeholic/autoloading/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'goodlife431/autoloading',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '023c50ac99a5fd422d66cd0a1e32658f2d2caade',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'goodlife431/autoloading' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '023c50ac99a5fd422d66cd0a1e32658f2d2caade',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
