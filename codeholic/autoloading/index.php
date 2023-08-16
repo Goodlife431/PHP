4b0ac6605e302e149e5668781a42eae327a01009
Goodlife <oduwolejohn431@gmail.com>
2023-08-16 10:30:21 +0100

    done with codeholic

--- codeholic/autoloading/index.php
+++ codeholic/autoloading/index.php
@@ -1,15 +1,20 @@
 <?php 
 
-// require_once './app/Email.php';
-// require_once './app/Person.php';
+
 require_once './vendor/autoload.php';
 
-use app\Email;
-use app\Person;
 
-$email = new Email();
 
-$person = new app\Person();
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
 
 ?>
