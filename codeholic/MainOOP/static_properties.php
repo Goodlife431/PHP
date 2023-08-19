e87c96b684b31a948992ed12a3e5f942fe9acaba
Goodlife431 <oduwolejohn431@gmail.com>
2023-08-19 13:46:33 +0100

    new~

---
+++ codeholic/MainOOP/static_properties.php
@@ -0,0 +1,13 @@
+<?php 
+ //static properties are associated to the class not instances while normal properties are asscoiated to the instance
+ 
+ class Car {
+    public $color = 'red';
+
+ }
+
+
+
+
+
+?>
