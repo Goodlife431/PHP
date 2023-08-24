708661fc79701399d6c7b841ad029c45ae3a200a
Goodlife431 <oduwolejohn431@gmail.com>
2023-08-24 12:44:33 +0100

    all about static methods and properties

--- codeholic/MainOOP/static_properties.php
+++ codeholic/MainOOP/static_properties.php
@@ -1,13 +1,84 @@
 <?php 
- //static properties are associated to the class not instances while normal properties are asscoiated to the instance
+//  static properties are associated to the class not instances while normal properties are asscoiated to the instance
  
- class Car {
-    public $color = 'red';
+ class Greetings {
+   public static function welcome(){
+      echo 'Good morning everyone';
+   }
 
+   public function __construct(){
+      self::welcome();
+   }
  }
 
+ new Greetings();
+//calling static methods 
+ //Greetings::welcome().PHP_EOL;
+
+
+class A{
+   public static function welcome(){
+      echo 'Welcome to this page';
+   }
+}
+
+class B{
+   public function message(){
+      A::welcome();
+   }
+}
+
+$obj = new B();
+echo $obj->message();
+
+
+class Domain {
+   protected static function getWebsiteName(){
+      return "W3schools";
+   }
+}
+
+class domainW3 extends Domain{
+   public $websiteName;
+
+   public function __construct() {
+      $this->websiteName = parent::getWebsiteName();
+   }
+}
+
+$obj = new domainW3();
+echo $obj->websiteName;
 
 
 
 
+
+class Pie{
+   public static $value= 3.1415;
+   public function staticValue(){
+      return self::$value;
+   }
+}
+
+$pi = new Pie();
+echo $pi->staticValue();
+
+
+class Pi {
+   public static $value = 3.145;
+
+}
+
+class Test extends Pi {
+   public $x;
+
+   public function xStatic(){
+      return parent::$value;
+   }
+}
+
+echo Test::$value;
+
+$obj = new Test();
+echo $obj->x;
 ?>
